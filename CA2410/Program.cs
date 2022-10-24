beb67e51a35045690227e2786d96200d3767f163
jeferson Ferrerira <Jeferson.oferreira@etec.sp.gov.br>
2022-10-24 20:34:20 -0300

    alteracoes c#

---
+++ CA2410/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2410
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int numero2;
+            int total;
+            numero = 10;
+            numero2 = 20;
+            total = numero + numero2;
+            Console.WriteLine("Boa noite!");
+            Console.WriteLine("Tecnico em informática");
+            Console.WriteLine(total);
+            Console.ReadKey();
+
+        }
+    }
+}
